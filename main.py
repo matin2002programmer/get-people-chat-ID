55b46740dc9acbea74bf936acdda1fe2c31c5453
matin2002programmer <citygame61@gmail.com>
2021-07-01 19:38:09 +0430

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import telegram
+
+bot_token="1809854554:AAFLZRo3ekBPbdQCNRXvqDYc2acoqCs962U"
+
+bot=telegram.Bot(bot_token)
+ 
+bot.get_updates()    
+chat_id  = bot.get_updates()[-1].message.chat_id
+people_id = chat_id
+
+x=1
+bot.send_message(chat_id,x)              
+
+                            
+        
+bot.send_message(chat_id,people_id)
